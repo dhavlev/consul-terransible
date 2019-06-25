64bf010e50f0013e3b16427de550484bcd0d2cd2
Dhavle <visharad.dhavle@here.com>
2019-06-25 16:12:09 +0530

    consul: terraform network updated

--- terraform/network/outputs.tf
+++ terraform/network/outputs.tf
@@ -1,23 +1,19 @@
-output "mw_vpc" {
-  value = "${aws_vpc.mw_vpc.id}"
+output "consul_vpc" {
+  value = "${aws_vpc.consul_vpc.id}"
 }
 
 output "web_security_group" {
-  value = "${aws_security_group.mw_sg_public.id}"
-}
-
-output "db_security_group" {
-  value = "${aws_security_group.mw_sg_private.id}"
+  value = "${aws_security_group.consul_sg_public.id}"
 }
 
 output "web_subnet_a" {
-  value = "${aws_subnet.mw_sub_public_a.id}"
+  value = "${aws_subnet.consul_sub_public_a.id}"
 }
 
 output "web_subnet_b" {
-  value = "${aws_subnet.mw_sub_public_b.id}"
+  value = "${aws_subnet.consul_sub_public_b.id}"
 }
 
-output "db_subnet" {
-  value = "${aws_subnet.mw_sub_private_a.id}"
+output "web_subnet_c" {
+  value = "${aws_subnet.consul_sub_public_c.id}"
 }
